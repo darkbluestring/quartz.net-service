98f478e0c87bdb0f024d662be1f8bc5f22e3147e
benoit texier <btexier@gmail.com>
2021-10-31 21:52:19 -0400

    add graphql

--- QuarztRestSrv/Program.cs
+++ QuarztRestSrv/Program.cs
@@ -7,12 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddRouting(o =>
-{
-    o.LowercaseUrls = true;
-
-});
-
+builder.Services.AddRouting();
+builder.Services.AddGraphQLServer();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -63,79 +59,7 @@ builder.Services.AddQuartz(q =>
     q.UseInMemoryStore();
     q.UseDefaultThreadPool(maxConcurrency: 10);
 
-    // quickest way to create a job with single trigger is to use ScheduleJob
-    /*q.ScheduleJob<ExampleJob>(trigger => trigger
-        .WithIdentity("Combined Configuration Trigger")
-        .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(7)))
-        .WithDailyTimeIntervalSchedule(x => x.WithInterval(10, IntervalUnit.Second))
-        .WithDescription("my awesome trigger configured for a job with single call")
-    );*/
-
-    // you can also configure individual jobs and triggers with code
-    // this allows you to associated multiple triggers with same job
-    // (if you want to have different job data map per trigger for example)
-    /* q.AddJob<ExampleJob>(j => j
-         .StoreDurably() // we need to store durably if no trigger is associated
-         .WithDescription("my awesome job")
-     );*/
-
-    // here's a known job for triggers
-    /*var jobKey = new JobKey("awesome job", "awesome group");
-    q.AddJob<ExampleJob>(jobKey, j => j
-        .WithDescription("my awesome job")
-    );
-
-    q.AddTrigger(t => t
-        .WithIdentity("Simple Trigger")
-        .ForJob(jobKey)
-        .StartNow()
-        .WithSimpleSchedule(x => x.WithInterval(TimeSpan.FromSeconds(10)).RepeatForever())
-        .WithDescription("my awesome simple trigger")
-    );
-
-    q.AddTrigger(t => t
-        .WithIdentity("Cron Trigger")
-        .ForJob(jobKey)
-        .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(3)))
-        .WithCronSchedule("0/3 * * * * ?")
-        .WithDescription("my awesome cron trigger")
-    );*/
-
-    // auto-interrupt long-running job
-    /* q.UseJobAutoInterrupt(options =>
-     {
-         // this is the default
-         options.DefaultMaxRunTime = TimeSpan.FromMinutes(5);
-     });
-     q.ScheduleJob<SlowJob>(
-         triggerConfigurator => triggerConfigurator
-             .WithIdentity("slowJobTrigger")
-             .StartNow()
-             .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever()),
-         jobConfigurator => jobConfigurator
-             .WithIdentity("slowJob")
-             .UsingJobData(JobInterruptMonitorPlugin.JobDataMapKeyAutoInterruptable, true)
-             // allow only five seconds for this job, overriding default configuration
-             .UsingJobData(JobInterruptMonitorPlugin.JobDataMapKeyMaxRunTime, TimeSpan.FromSeconds(5).TotalMilliseconds.ToString(CultureInfo.InvariantCulture)));
-
-     const string calendarName = "myHolidayCalendar";
-     q.AddCalendar<HolidayCalendar>(
-         name: calendarName,
-         replace: true,
-         updateTriggers: true,
-         x => x.AddExcludedDate(new DateTime(2020, 5, 15))
-     );
-
-     q.AddTrigger(t => t
-         .WithIdentity("Daily Trigger")
-         .ForJob(jobKey)
-         .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(5)))
-         .WithDailyTimeIntervalSchedule(x => x.WithInterval(10, IntervalUnit.Second))
-         .WithDescription("my awesome daily time interval trigger")
-         .ModifiedByCalendar(calendarName)
-     );
-     */
-    // also add XML configuration and poll it for changes
+    // add XML configuration and poll it for changes
     q.UseXmlSchedulingConfiguration(x =>
     {
         x.Files = new[] { "quartz_jobs.xml" };
@@ -190,6 +114,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
 
 app.UseHttpsRedirection();
 
